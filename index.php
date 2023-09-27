84fcfdbaec360ca7fb6c08ba0a5b76c7e2009b03
Yous <Eljilaliyousra@gmail.com>
2023-09-27 15:22:38 +0200

    Nouveau router

--- index.php
+++ index.php
@@ -8,11 +8,12 @@ require_once 'models/functions/UserModel.php';
 
 
 session_start();
+// ↓ Initialize les variables $view, $css et $js
+require_once 'controllers/routerNew.php';
 
+// ↓ Template
 require_once 'views/templates/header.php';
 
 require_once 'views/templates/nav.php';
-
-require_once 'controllers/router.php';
 
 require_once 'views/templates/footer.php';
