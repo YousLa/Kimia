9b41e287718c4ab0ea8a834d91c1da00589caeaa
Yous <Eljilaliyousra@gmail.com>
2023-10-04 17:12:47 +0200

    Home - CSS - Responsive

--- views/pages/homeView.php
+++ views/pages/homeView.php
@@ -1,87 +1,87 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- Section 1 -->
+<section class="container-section1">
+    <div id="section1">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Home</title>
-
-</head>
-
-<body>
-
-</body>
-
-</html>
+        <p>Embarquez pour un voyage narratif avec Kimia, la plateforme de streaming de contes pour tous.</p>
 
 
-<main>
+        <div class="commencer">
 
-    <!-- Section 1 -->
-    <section class="hero-section">
-        <div id="section1">
+            <div class="input-container">
 
-            <p>Embarquez pour un voyage narratif avec Kimia, la plateforme de streaming de contes pour tous.</p>
+                <div class="input-field">
 
+                    <div id="interne">
+                        <input type="text" id="email">
+                        <label for="email">Adresse e-mail</label>
+                    </div>
 
-            <div class="commencer">
-                <input class="input-forms" type="text" placeholder="Adresse Email">
+                </div>
+
                 <button id='signup'><a href='?page=signup'>Commencer</a></button>
+
             </div>
 
         </div>
 
+    </div>
+
+</section>
+
+<!-- Main - taille width -->
+<main>
+    <div id="only">
+
+        <img id="arrow" src="assets/img/homepage/arrow.svg" alt="">
+
         <h1>Seulement sur Kimia</h1>
-
-    </section>
-
-
+    </div>
     <!-- Section 2 -->
     <div id="section2">
 
+
         <h2>Collection diversifiée de contes</h2>
 
-        <p>Kimia se distingue par sa vaste collection de contes provenant de cultures du monde entier.</p>
+        <p class="centrer-p">Kimia se distingue par sa vaste collection de contes provenant de cultures du monde entier.</p>
 
         <!-- Preview-->
 
         <div class="slideshow-container">
             <div class="slide">
-                <img src="assets/img/homepage/slide-accueil/Slide1.png" alt="Image 1">
+                <img class="halo" src="assets/img/homepage/slide-accueil/Slide1.png" alt="Image 1">
             </div>
             <div class="slide">
-                <img src="assets/img/homepage/slide-accueil/Slide2.png" alt="Image 2">
+                <img class="halo" src="assets/img/homepage/slide-accueil/Slide2.png" alt="Image 2">
             </div>
             <div class="slide">
-                <img src="assets/img/homepage/slide-accueil/Slide3.png" alt="Image 3">
+                <img class="halo" src="assets/img/homepage/slide-accueil/Slide3.png" alt="Image 3">
             </div>
 
             <div class="slide">
-                <img src="assets/img/homepage/slide-accueil/Slide4.png" alt="Image 4">
+                <img class="halo" src="assets/img/homepage/slide-accueil/Slide4.png" alt="Image 4">
             </div>
             <div class="slide">
-                <img src="assets/img/homepage/slide-accueil/Slide5.png" alt="Image 5">
+                <img class="halo" src="assets/img/homepage/slide-accueil/Slide5.png" alt="Image 5">
             </div>
             <div class="slide">
-                <img src="assets/img/homepage/slide-accueil/Slide6.png" alt="Image 6">
+                <img class="halo" src="assets/img/homepage/slide-accueil/Slide6.png" alt="Image 6">
             </div>
             <div class="slide">
-                <img src="assets/img/homepage/slide-accueil/Slide7.png" alt="Image 7">
+                <img class="halo" src="assets/img/homepage/slide-accueil/Slide7.png" alt="Image 7">
             </div>
             <div class="slide">
-                <img src="assets/img/homepage/slide-accueil/Slide8.png" alt="Image 8">
+                <img class="halo" src="assets/img/homepage/slide-accueil/Slide8.png" alt="Image 8">
             </div>
             <div class="slide">
-                <img src="assets/img/homepage/slide-accueil/Slide9.png" alt="Image 9">
+                <img class="halo" src="assets/img/homepage/slide-accueil/Slide9.png" alt="Image 9">
             </div>
         </div>
 
         <h2>Contenus exclusifs et originaux</h2>
 
-        <p>Kimia propose une série de contes exclusifs et originaux produits en interne, offrant ainsi aux utilisateurs un accès à des histoires uniques et captivantes qu'ils ne trouveront nulle part ailleurs.</p>
-
+        <p class="centrer-p">Kimia propose une série de contes exclusifs et originaux produits en interne, offrant ainsi aux utilisateurs un accès à des histoires uniques et captivantes qu'ils ne trouveront nulle part ailleurs.</p>
     </div>
+
 
     <!-- Section 3 -->
     <div id="section3">
@@ -97,9 +97,22 @@
             <img src="assets/img/homepage/plateformes/Kimia-phone.svg" alt="">
         </div>
 
-        <div class="commencer">
-            <input class="input-forms" type="text" placeholder="Adresse Email">
-            <button id='signup'><a href='?page='>Commencer</a></button>
+        <div class="commencer bottom">
+
+            <div class="input-container">
+
+                <div class="input-field">
+
+                    <div id="interne">
+                        <input type="text" id="email">
+                        <label for="email">Adresse e-mail</label>
+                    </div>
+
+                </div>
+
+                <button id='signup'><a href='?page=signup'>Commencer</a></button>
+
+            </div>
         </div>
     </div>
 
--- views/templates/footer.php
+++ views/templates/footer.php
@@ -3,9 +3,9 @@
     <!-- TODO Résoudre le soucis avec le fond du logo -->
     <a href='?page=home'><img id="logo-footer" src="assets/img/logo/Kimia-purple.svg" alt=""></a>
     <div>
-        <p>Nous contacter</p>
-        <p>Conditions d'utilisation</p>
-        <p>À propos</p>
+        <p><a href="#">Nous contacter</a> </p>
+        <p><a href="#">Conditions d'utilisation</a></p>
+        <p><a href="#">À propos</a></p>
     </div>
 </footer>
 
