208ec4366294e016bfa80d4d53f1249ffa9e940b
Mbiie <minbayot@yahoo.fr>
2023-10-13 16:52:20 +0200

    pages about + rgdp + contact

--- views/pages/contactView.php
+++ views/pages/contactView.php
@@ -1,1 +1,52 @@
-<p>PAGE CONTACT</p>
+
+
+
+<section class="contact-hero-section">
+
+
+<div class="contact-form-box">
+
+
+<form action="?page=contact" method="POST">
+
+<h1 class="loginh1">Nous contacter</h1>
+
+
+    <div class="contact-input-box">
+
+    <label for="fname">Prénom</label>
+    <input type="text" id="fname" name="firstname" >
+
+    </div>
+
+    
+    <div class="contact-input-box">
+
+    <label for="lname">Nom</label>
+    <input type="text" id="lname" name="lastname" >
+
+    </div>
+
+    
+    <div class="contact-input-box1">
+    <label for="subject">Votre message</label>
+    <textarea id="subject" name="subject" style="height:100px" class="textarea" ></textarea>
+
+
+    </div>
+
+    <div class="submit">
+
+    <input type="submit" value="Envoyer">
+
+    </div>
+
+
+    
+</div> 
+
+
+</form>
+
+
+</section>
